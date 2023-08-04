32237ce694881178ec025582c85f458ca70f0427
Naveen <imnaveenbharath@gmail.com>
2023-08-04 06:43:40 +0530

    oviya torture

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,51 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
+
+import Note from "./models/Note.js";
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 
-
-// - GET
-app.get("/hello", function (req, res) {
-  res.send("Hello");
-});
-
-
-
-// - POST
-app.post("/", function (req, res) {
+app.post("/create", function (req, res) {
   const data = req.body;
-  console.log(data);
+
+  const entry = new Note(data);
+  entry.save();
+
   res.status(200).send();
 });
 
+app.get("/getNotes", function (req, res) {
+  Note.find({}).then((data) => {
+    res.send(data);
+    console.log(data);
+  });
+});
+
+app.delete("/removeNote/:id", function (req, res) {
+  const noteId = req.params.id;
+  Note.findByIdAndDelete(noteId)
+    .then(() => {
+      res.send("Note deleted successfully");
+    })
+    .catch((error) => {
+      console.error("Error deleting note:", error);
+      res.status(500).send("Error deleting note");
+    });
+});
 
 
-// Server Running in PORT 8000
-app.listen(8000);
+mongoose
+  .connect(
+    "mongodb+srv://root:PNEoZEIU5ajO6EQp@test.jz2qknw.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    // Server Running in PORT 8000
+    app.listen(8000, () => console.log("Server is running"));
+  })
+  .catch((err) => {
+    console.log(err);
+  });
