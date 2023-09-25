adc5d2f5b93feb36ec1c59e7d0435243842c99de
Naveen <imnaveenbharath@gmail.com>
2023-09-25 10:24:28 +0530

    Revert "all functionalities added"
    This reverts commit c316e34749697251b24bbcbc9d8c4f22a6784995.

--- backend/index.js
+++ backend/index.js
@@ -56,19 +56,6 @@ app.put("/todos/:id", async (req, res) => {
   }
 });
 
-app.delete("/todos/:id", async (req, res) => {
-  try {
-    const todoId = req.params.id;
-
-    const deletedTodo = await Todo.findByIdAndDelete(todoId);
-    res.json(deletedTodo);
-  } catch (err) {
-    res
-      .status(400)
-      .json({ message: "Failed to delete Todo item", error: err.message });
-  }
-});
-
 app.patch("/todos/:id/done", async (req, res) => {
   try {
     const todoId = req.params.id;
